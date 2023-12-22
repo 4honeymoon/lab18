a87268d1259cfaeb1b3d02a497f88d294142cabb
4honeymoon <mkhutinaeva@bk.ru>
2023-12-23 00:06:55 +0300

    Initial commit

---
+++ src/Task6.java
@@ -0,0 +1,22 @@
+public class ThrowsDemo2 {
+    public static void printMessage(String key) {
+        String message = getDetails(key);
+        System.out.println(message);
+    }
+
+    public static String getDetails(String key) {
+        if (key == null) {
+            throw new NullPointerException("null key in getDetails");
+        }
+        return "data for " + key;
+    }
+
+    public static void main(String[] args) {
+        printMessage("Hello");
+        try {
+            printMessage(null);
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException" + e.getMessage());
+        }
+    }
+}
